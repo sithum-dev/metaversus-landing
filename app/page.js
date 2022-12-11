9f7f8bf3304590c24184179c24e4f8e22791a216
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-12-11 10:02:51 +0530

    Explore main setup

--- app/page.js
+++ app/page.js
@@ -24,14 +24,12 @@ const Page = () => (
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
-
     <World />
     <div className="relative">
       <Insights />
       <div className="gradient-04 z-0" />
       <Feedback />
     </div>
-
     <Footer />
   </div>
 );
